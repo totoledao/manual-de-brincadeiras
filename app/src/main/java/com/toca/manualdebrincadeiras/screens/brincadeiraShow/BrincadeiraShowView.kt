4de5a6a04079196f82be6d52c50b47cf664463a8
Guilherme Toledo <totoledao@hotmail.com>
2024-06-05 16:08:14 -0300

    Add dynamic image rendering for game description

--- app/src/main/java/com/toca/manualdebrincadeiras/screens/brincadeiraShow/BrincadeiraShowView.kt
+++ app/src/main/java/com/toca/manualdebrincadeiras/screens/brincadeiraShow/BrincadeiraShowView.kt
@@ -1,5 +1,7 @@
 package com.toca.manualdebrincadeiras.screens.brincadeiraShow
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -18,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
 
 @Composable
@@ -50,12 +53,12 @@ fun BrincadeiraShowView(
                 }
             }
             item {
-                Column() {
-                    Text(text = state.brincadeira?.id.toString() ?: "")
+                Column {
+                    Text(text = state.brincadeira?.id.toString())
                     Text(text = state.brincadeira?.nome ?: "")
-                    Text(text = state.brincadeira?.idade_min.toString() ?: "")
-                    Text(text = state.brincadeira?.idade_max.toString() ?: "")
-                    Text(text = state.brincadeira?.descricao ?: "")
+                    Text(text = state.brincadeira?.idade_min.toString())
+                    Text(text = state.brincadeira?.idade_max.toString())
+                    WebViewScreen(state.brincadeira?.descricao ?: "")
                 }
             }
 
@@ -72,5 +75,48 @@ fun FavoriteIcon(isFavorite: Boolean) {
             Icons.Filled.FavoriteBorder
         },
         contentDescription = "Favorite",
+    )
+}
+
+
+@Composable
+fun WebViewScreen(descricao: String) {
+
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = false
+                settings.loadWithOverviewMode = false
+                settings.useWideViewPort = false
+                settings.setSupportZoom(false)
+            }
+        },
+        update = { webView ->
+            val unEncodedHtml = """
+                <html>
+                <head>
+                        <style>
+                            img {
+                                width: 100%;
+                                height: auto;
+                                display: block;
+                                margin: 0 auto;
+                            }
+                        </style>
+                    </head>
+                    <body style='margin: 0; padding: 0;'>
+                        $descricao                        
+                    </body>
+                </html>
+            """.trimIndent()
+            webView.loadDataWithBaseURL(
+                "file:///android_asset/images/",
+                unEncodedHtml,
+                "text/html",
+                "UTF-8",
+                null
+            )
+        }
     )
 }
